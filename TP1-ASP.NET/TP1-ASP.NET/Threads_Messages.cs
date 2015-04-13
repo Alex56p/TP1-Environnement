3331096b6d9d80989cff3ed71d8388693fd67621
PoneyLoverXz <antho.voyez@hotmail.com>
2015-04-13 12:31:40 -0400

    Refait la BD

--- TP1-ASP.NET/TP1-ASP.NET/Threads_Messages.cs
+++ TP1-ASP.NET/TP1-ASP.NET/Threads_Messages.cs
@@ -5,8 +5,19 @@ using System.Web;
 
 namespace TP1_ASP.NET
 {
-    public class Threads_Messages
+    public class Threads_Messages : SqlExpressUtilities.SqlExpressWrapper
     {
+        public int id;
+        public int Thread_ID;
+        public int User_ID;
+        public DateTime Date_of_Creation;
+        public String Message;
 
+
+        public Threads_Messages(String connexionString, System.Web.UI.Page Page)
+            : base(connexionString, Page)
+        {
+            SQLTableName = "THREADS_MESSAGES";
+        }
     }
 }
